73144b1be05e3bab26efa6514f0d8c4cca413000
Rolland <Rollandcampbell@msn.com>
2019-10-10 09:12:23 -0600

    adjusted room constructor

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -2,15 +2,35 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Game : IGame
-    {
-        public IRoom CurrentRoom { get; set; }
-        public IPlayer CurrentPlayer { get; set; }
+  public class Game : IGame
+  {
+    public IRoom CurrentRoom { get; set; }
+    public IPlayer CurrentPlayer { get; set; }
 
-        //NOTE Make yo rooms here...
-        public void Setup()
-        {
-            throw new System.NotImplementedException();
-        }
+    //NOTE Make yo rooms here...
+    public void Setup()
+    {
+      //Creating Rooms
+      Room Room0 = new Room("Room 0", "desc0");
+      Room Room1 = new Room("Room 1", "desc1");
+      Room Room2 = new Room("Room 2", "desc2");
+      Room Room3 = new Room("Room 3", "desc3");
+
+      //Connecting Rooms
+      Room0.AddExits(Room1);
+
+      Room1.AddExits(Room0);
+      Room1.AddExits(Room2);
+
+      Room2.AddExits(Room3);
+      Room2.AddExits(Room1);
+
+      Room3.AddExits(Room2);
+
+      //Add items to Rooms
+      Item Item1 = new Item("sword", "A glorious Claymore, trimmed in gold");
+
+      CurrentRoom = Room0;
     }
+  }
 }
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -3,11 +3,23 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Room : IRoom
+  public class Room : IRoom
+  {
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Item> Items { get; set; }
+    public Dictionary<string, IRoom> Exits { get; set; }
+
+    public void AddExits(IRoom room)
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Item> Items { get; set; }
-        public Dictionary<string, IRoom> Exits { get; set; }
+      Exits.Add(room.Name, room);
     }
+
+    public Room(string name, string description)
+    {
+      Name = name;
+      Description = description;
+    }
+  }
 }
